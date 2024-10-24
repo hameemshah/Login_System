7eb816808562075408c4b908b75f71133ee069df
hameemshah <muhammad.hameem.shah@gmail.com>
2024-10-24 12:39:18 +0530

    Charset Update

--- partials/config.php
+++ partials/config.php
@@ -11,4 +11,4 @@ DEFINE ('DB_NAME', 'users');
 //Make the connection
 $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL :' . mysqli_connect_error());
 //Set the encoding
-mysqli_set_charset($dbc, 'utf-8');
+mysqli_set_charset($dbc, 'utf8mb4');
